dfbf9c3aacb6e41deb6c72425e4e24a2cd4235ec
Sebastian Collazo-Quaas <mission@collazo.de>
2021-05-16 14:05:14 +0200

    Add PageNotFound view and navigation guards

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import store from "@/store/index";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,15 +10,39 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/login",
     name: "Login",
-    meta: { layout: "fullwidth-with-footer" },
+    meta: {
+      layout: "fullwidth-with-footer",
+      needsToBeUnauthenticated: true,
+    },
     component: () =>
       import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "PageNotFound",
+    component: () =>
+      import(
+        /* webpackChunkName: "pagenotfound" */ "../views/PageNotFound.vue"
+      ),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.needsToBeUnauthenticated && !store.getters.isAuthenticated) {
+    next("/login");
+  } else if (
+    to.meta.needsToBeUnauthenticated &&
+    store.getters.isAuthenticated
+  ) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
